89d2c970ff0e82fd046e4445204400321d8b3441
strannic123 <manager350@mail.ru>
2021-12-15 17:32:58 +0300

    first commit added normalize.css

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    todo: []
+  },
+  getters: {
+    getTodo: state => state.todo,
+    getTodoLength: state => state.todo.length
   },
   mutations: {
+    ADD_TODO_ITEM(state, payload) {
+      state.todo.push(payload)
+    },
+    EDIT_CHANGE(state, index) {
+      state.todo[index].edit = !state.todo[index].edit
+    }
   },
   actions: {
+    addTodoItem({commit}, payload) {
+      commit('ADD_TODO_ITEM', payload)
+    },
+    editChange ({commit}, index) {
+      commit('EDIT_CHANGE', index)
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,110 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+   <div class="wrapper wrapper-input">
+     <div class="content">
+       <label >
+         <input
+             class="input-add"
+             @keydown.enter="addItem"
+             type="text"
+             placeholder="What needs to be done ?"
+             v-model="todoInput">
+       </label>
+     </div>
+   </div>
+
+  <div class="wrapper wrapper-todo-list">
+    <div class="content">
+      <div class="todo-list" v-for="todo in storeTodo" :key="todo.id">
+        <div
+            class="todo-item"
+            @dblclick="editTodo(todo)"
+            v-if="!todo.edit"
+        >
+          {{todo.text}}
+          {{todo.id}}
+        </div>
+        <div
+            v-else>
+          <input
+              v-focus
+              type="text"
+              @blur="editDone(todo)"
+              @keydown.enter="editDone(todo)"
+              v-model="todo.text"
+              class="input-edit">
+        </div>
+      </div>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  data() {
+    return {
+      todoInput: '',
+      todoID: 0
+    }
+  },
+  computed: {
+    todoId() {
+      return this.$store.getters['getTodoLength']
+    },
+    storeTodo() {
+      return this.$store.state.todo
+    }
+  },
+  methods: {
+    addItem() {
+      this.$store.dispatch('addTodoItem', {
+        id: ++this.todoID ,
+        text: this.todoInput,
+        completed: false,
+        edit: false
+      })
+      this.todoInput = ''
+    },
+    editTodo (todo) {
+      todo.edit = true
+    },
+    editDone (todo) {
+      todo.edit = false
+    }
+  },
+  directives: {
+    focus: {
+      inserted(el) {
+        el.focus()
+      }
+    }
   }
 }
 </script>
+
+<style scoped>
+.container {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.wrapper {
+  width: 350px;
+}
+.input-add,
+.input-edit {
+  width: 100%;
+  padding: 0;
+}
+.todo-list {
+  border: 1px solid darkgray;
+}
+</style>
